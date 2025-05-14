10139e194770c91029b6df6b2bfc7110b796b26d
Christoph Sander <74921235+ch-sander@users.noreply.github.com>
2025-05-14 10:04:40 +0200

    cors

--- src/zotero_rdf_server/main.py
+++ src/zotero_rdf_server/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from .lifespan import app_lifespan
 from .api import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(lifespan=app_lifespan, docs_url="/")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # oder deine genaue Herkunft
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
