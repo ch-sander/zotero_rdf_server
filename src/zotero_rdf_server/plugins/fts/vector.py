1a67138c1de091a9aff0c932b09ad37abb7a79f0
Christoph Sander <74921235+ch-sander@users.noreply.github.com>
2026-02-07 20:12:53 +0100

    suppress logs

--- src/zotero_rdf_server/plugins/fts/vector.py
+++ src/zotero_rdf_server/plugins/fts/vector.py
@@ -1,9 +1,17 @@
-from .helpers import ensure_import, clean_ocr, plugin_logger
+from .helpers import ensure_import
 ensure_import("sentence_transformers==5.2.2", requirements=None)
-from sentence_transformers import SentenceTransformer
 
 _model = None
+import os
+os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
+os.environ["TQDM_DISABLE"] = "1"
 
+import logging
+logging.getLogger("sentence_transformers").setLevel(logging.ERROR)
+logging.getLogger("transformers").setLevel(logging.ERROR)
+logging.getLogger("huggingface_hub").setLevel(logging.ERROR)
+
+from sentence_transformers import SentenceTransformer
 def embed(text: str) -> list[float]:
     global _model
     if _model is None:
@@ -11,6 +19,7 @@ def embed(text: str) -> list[float]:
     vec = _model.encode(
         text,
         normalize_embeddings=True,
-        prompt="passage: "
+        prompt="passage: ",
+        show_progress_bar=False
     )
     return vec.tolist()
