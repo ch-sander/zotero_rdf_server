8f120a151bd7a03f03847a4801dbfd95fc6435dd
ch-sander <c.sander1510@gmail.com>
2026-03-10 13:17:31 +0100

    improved OS index alias
    not tested

--- src/zotero_rdf_server/plugins/fts/endpoints.py
+++ src/zotero_rdf_server/plugins/fts/endpoints.py
@@ -592,7 +592,6 @@ class KeywordFilter(BaseModel):
     filter_value: Optional[str] = None
     filter_values: Optional[List[str]] = None
 
-
 def keyword_filter_params(
     filter_field: Optional[str] = Query(
         None,
@@ -765,13 +764,10 @@ def format_search_response(
 
 from enum import Enum
 
-
-
 class OutputFormat(str, Enum):
     json = "json"
     csv = "csv"
     markdown = "markdown"
-
 
 class AggType(str, Enum):
     terms = "terms"
@@ -789,8 +785,8 @@ class AggType(str, Enum):
 def search_terms(
     request: Request,
     index: str = Query(
-        ...,
-        description="Name of the OpenSearch index to query.",
+        None,
+        description="Name of the OpenSearch index to query. Default alias can be set in cofiguration.",
     ),
     q: str = Query(
         ...,
@@ -1056,7 +1052,7 @@ def search_terms(
 )
 def search_proximity(
     request: Request,
-    index: str = Query(..., description="OpenSearch index name"),
+    index: str = Query(None, description="OpenSearch index name. Default alias can be set in cofiguration"),
     a: str = Query(..., description="CSV list A"),
     b: str = Query(..., description="CSV list B"),
     field: str = Query("text", description="Text field to search"),
@@ -1133,7 +1129,7 @@ def search_proximity(
 )
 def knn_by_id(
     request: Request,
-    index: str = Query(..., description="OpenSearch index name"),
+    index: str = Query(None, description="OpenSearch index name. Default alias can be set in cofiguration"),
     os_id: str = Query(..., description="Reference document OpenSearch _id"),
     vector_field: str = Query("vector", description="knn_vector field name"),
     k: int = Query(50, ge=1, le=10000),
@@ -1154,7 +1150,7 @@ def knn_by_id(
     out_header: OutputHeader = Depends(output_header_params),
     debug: bool = Query(False, description="Include debug_query (JSON only)"),
 ):
-    from .search import get_doc_vector, os_search, apply_paging
+    from .search import get_doc_vector, os_search, apply_paging, apply_keyword_filter
     api_call = str(request.url) 
     try:
         query_vec = get_doc_vector(index=index, os_id=os_id, vector_field=vector_field)
@@ -1181,6 +1177,7 @@ def knn_by_id(
 
     body: Dict[str, Any] = {"query": query}
 
+    apply_keyword_filter(body, filters)
     apply_paging(body, size=size)
 
     try:
@@ -1210,7 +1207,7 @@ def knn_by_id(
 )
 def mlt_by_id(
     request: Request,
-    index: str = Query(..., description="OpenSearch index name"),
+    index: str = Query(None, description="OpenSearch index name. Default alias can be set in cofiguration"),
     os_id: str = Query(..., description="Reference document OpenSearch _id"),
     fields: str = Query("text", description="CSV list of fields, typically 'text'"),
     min_term_freq: int = Query(1, ge=0),
