02efb24cf64d0c8a8af3af0021c9d6dd252eaf4a
Christoph Sander <74921235+ch-sander@users.noreply.github.com>
2026-07-23 23:29:39 +0200

    prefixes

--- src/zotero_rdf_server/config.py
+++ src/zotero_rdf_server/config.py
@@ -303,4 +303,4 @@ ZOT_BASE_URL = ZOTERO_CONFIGS.get("base_url", "https://zotero.org/")
 ZOT_SCHEMA = ZOTERO_CONFIGS.get("schema") # "https://api.zotero.org/schema"
 REGEX_PATTERN = f"{ZOT_NS}regex"
 
-PREFIXES = {"zot":ZOT_NS, "rdfs":"http://www.w3.org/2000/01/rdf-schema#", "owl":"http://www.w3.org/2002/07/owl#", "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#", "xsd":XSD_NS, "skos":"http://www.w3.org/2004/02/skos/core#", "prov":"http://www.w3.org/ns/prov#", "dc":"http://purl.org/dc/elements/1.1/", "schema":"https://schema.org/", "dct":"http://purl.org/dc/terms/", "zmap": MAPPING_BASE}
+PREFIXES = {"zot":ZOT_NS, "rdfs":"http://www.w3.org/2000/01/rdf-schema#", "owl":"http://www.w3.org/2002/07/owl#", "rdf":"http://www.w3.org/1999/02/22-rdf-syntax-ns#", "xsd":XSD_NS, "skos":"http://www.w3.org/2004/02/skos/core#", "prov":"http://www.w3.org/ns/prov#", "dc":"http://purl.org/dc/elements/1.1/", "schema":"https://schema.org/", "dct":"http://purl.org/dc/terms/", "zmap": MAPPING_BASE, "semz": "https://semantic-html.org/vocab#", "foaf": "http://xmlns.com/foaf/0.1/", "geo": "http://www.opengis.net/ont/geosparql#", "wd": "http://www.wikidata.org/entity/"}
